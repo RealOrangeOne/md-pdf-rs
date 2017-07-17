c83d84705150500c96322a905265e084e1b6904a
Jake Howard <git@theorangeone.net>
2017-07-17 20:34:20 +0100

    unwrap multiple items

--- src/config/validate.rs
+++ src/config/validate.rs
@@ -1,5 +1,21 @@
 use config::Config;
 
-pub fn validate(config: Config) {
+use std::vec::Vec;
 
+
+pub fn unwrap_group(config: &Config, funcs: Vec<&Fn(&Config) -> Result<(), String>>) -> Result<(), String> {
+    for func in funcs.iter() {
+        let func_result = func(config);
+        if func_result.is_err() {
+            return Err(func_result.unwrap_err());
+        }
+    }
+    return Ok(());
+}
+
+
+pub fn validate(config: Config) -> Result<(), String> {
+    return unwrap_group(&config, vec!(
+        &|c| { Ok(()) }
+    ));
 }
