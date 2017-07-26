70c48ea9abb8328f1701ab9e908b03db2d323326
Jake Howard <git@theorangeone.net>
2017-07-26 15:28:44 +0100

    inline result utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 
+#[inline]
 pub fn result_override<T, E: Debug>(r: Result<T, E>, msg: String) -> Result<T, String> {
     return match r {
         Ok(t) => Ok(t),
@@ -7,7 +8,8 @@ pub fn result_override<T, E: Debug>(r: Result<T, E>, msg: String) -> Result<T, S
     };
 }
 
-pub fn result_prefix<T: Debug, E: Debug>(r: Result<T, E>, prefix: String) -> Result<T, String> {
+#[inline]
+pub fn result_prefix<T, E: Debug>(r: Result<T, E>, prefix: String) -> Result<T, String> {
     return match r {
         Ok(t) => Ok(t),
         Err(e) => Err(format!("{}: {:?}", prefix, e))
