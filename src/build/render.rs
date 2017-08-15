c8471db60f8b2d7354b515e5e83150aa83995e15
Jake Howard <git@theorangeone.net>
2017-08-15 22:47:39 +0100

    Fix format

--- src/build/render.rs
+++ src/build/render.rs
@@ -7,9 +7,7 @@ fn stub(config: Config, input: String) -> Result<String, String> {
 
 pub fn render(config: Config, input: String) -> Result<String, String> {
     let mut rendered_input = input;
-    for renderer in vec![
-        stub
-    ] {
+    for renderer in vec![stub] {
         rendered_input = try!(renderer(config.clone(), rendered_input));
     }
     return Ok(rendered_input);
