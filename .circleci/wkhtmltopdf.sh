60e46dfcb6d8d672b13fae1bdd3cb86fbc440fc8
Jake Howard <git@theorangeone.net>
2017-08-27 23:13:27 +0100

    Script merge

--- .circleci/wkhtmltopdf.sh
+++ .circleci/wkhtmltopdf.sh
@@ -4,7 +4,14 @@
 
 set -e
 
-apt-get install wkhtmltopdf xvfb -y
+apt-get install xvfb -y
+
+wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar -xJf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+cd wkhtmltox
+chown root:root bin/wkhtmltopdf
+cp -r * /usr/
+
 echo -e '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
 chmod a+x /usr/bin/wkhtmltopdf.sh
 ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
