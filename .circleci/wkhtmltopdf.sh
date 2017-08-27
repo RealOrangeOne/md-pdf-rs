8daa137fb5f22a1f260c1bf27f65b28312b7b24a
Jake Howard <git@theorangeone.net>
2017-08-27 22:30:09 +0100

    Install wkhtmltopdf on CI

---
+++ .circleci/wkhtmltopdf.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# 'stolen' from https://github.com/JazzCore/python-pdfkit/blob/master/travis/before-script.sh
+
+set -e
+
+apt-get install -y openssl build-essential xorg libssl-dev
+wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar -xJf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+cd wkhtmltox
+chown root:root bin/wkhtmltopdf
+cp -r * /usr/
