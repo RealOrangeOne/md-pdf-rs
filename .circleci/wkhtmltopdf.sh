6267e12c9fe23ca2e2d4a9ec28830976f81687a7
Jake Howard <git@theorangeone.net>
2017-08-27 23:21:53 +0100

    Install from ppa too

--- .circleci/wkhtmltopdf.sh
+++ .circleci/wkhtmltopdf.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-apt-get install xvfb -y
+apt-get install wkhtmltopdf xvfb -y
 
 wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 tar -xJf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
