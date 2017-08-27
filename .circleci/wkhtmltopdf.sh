ed6f992868f9e4dd52738717ed4d2054a5cd462d
Jake Howard <git@theorangeone.net>
2017-08-27 23:16:28 +0100

    Dont force quiet

--- .circleci/wkhtmltopdf.sh
+++ .circleci/wkhtmltopdf.sh
@@ -12,6 +12,6 @@ cd wkhtmltox
 chown root:root bin/wkhtmltopdf
 cp -r * /usr/
 
-echo -e '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
+echo -e '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf $*' > /usr/bin/wkhtmltopdf.sh
 chmod a+x /usr/bin/wkhtmltopdf.sh
 ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
