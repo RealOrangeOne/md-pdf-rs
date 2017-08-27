70154f87a5a838a7ce10fc7160fcea4bcb89258c
Jake Howard <git@theorangeone.net>
2017-08-27 23:03:04 +0100

    Accept install

--- .circleci/wkhtmltopdf.sh
+++ .circleci/wkhtmltopdf.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-apt-get install wkhtmltopdf xvfb
+apt-get install wkhtmltopdf xvfb -y
 echo -e '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
 chmod a+x /usr/bin/wkhtmltopdf.sh
 ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
