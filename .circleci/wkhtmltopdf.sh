f673c596c0249af9b64866563dd3b4fc473dd1d6
Jake Howard <git@theorangeone.net>
2017-08-27 23:00:13 +0100

    Try new script for wkhtmltopdf install

--- .circleci/wkhtmltopdf.sh
+++ .circleci/wkhtmltopdf.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# 'stolen' from https://github.com/JazzCore/python-pdfkit/blob/master/travis/before-script.sh
+# https://github.com/JazzCore/python-pdfkit/wiki/Using-wkhtmltopdf-without-X-server
 
 set -e
 
-apt-get install -y openssl build-essential libssl-dev
-wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-tar -xJf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-cd wkhtmltox
-chown root:root bin/wkhtmltopdf
-cp -r * /usr/
+apt-get install wkhtmltopdf xvfb
+echo -e '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
+chmod a+x /usr/bin/wkhtmltopdf.sh
+ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
