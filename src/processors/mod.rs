bb43ebad70a33113b04005ccb032ceb5330088f1
Jake Howard <git@theorangeone.net>
2017-09-06 19:50:39 +0100

    Rename function so it makes more sense

--- src/processors/mod.rs
+++ src/processors/mod.rs
@@ -1,8 +1,8 @@
 use config::Config;
 
-pub mod html_cleanup;
+pub mod head_cleanup;
 pub mod strip_blank;
 
 
 pub const PROCESSORS: [fn(Config, String) -> Result<String, String>; 2] =
-    [html_cleanup::html_cleanup, strip_blank::strip_blank];
+    [head_cleanup::head_cleanup, strip_blank::strip_blank];
