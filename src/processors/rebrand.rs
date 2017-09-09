ac4c7ed0d0ff65d8989ab2bf6556b495247624bc
Jake Howard <git@theorangeone.net>
2017-09-09 18:02:03 +0100

    Fix rebrand function warnings

--- src/processors/rebrand.rs
+++ src/processors/rebrand.rs
@@ -5,7 +5,7 @@ use config::Config;
 pub fn rebrand(config: Config, input: String) -> Result<String, String> {
     let mut root = sciter_start(input);
     let mut head = get_head(&mut root);
-    let mut ele = find_first(&mut root, "meta[name='generator']");
-    ele.set_attribute("content", crate_name!());
+    let mut ele = find_first(&mut head, "meta[name='generator']");
+    ele.set_attribute("content", crate_name!()).expect("Failed to set generator.");
     return Ok(get_html(root));
 }
