0e03bc0740b2be41821bb5e54923ab82bf61d5e9
Jake Howard <git@theorangeone.net>
2017-09-10 19:19:05 +0100

    pass references data into pandoc

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -5,7 +5,7 @@ use config::Config;
 
 
 pub fn build_input(config: Config, input: String) -> Result<String, String> {
-    let html = try!(pandoc::render(input));
+    let html = try!(pandoc::render(config.clone(), input));
     let rendered = try!(process::render(config.clone(), html));
     return Ok(rendered);
 }
--- src/build/pandoc.rs
+++ src/build/pandoc.rs
@@ -2,9 +2,10 @@ use pandoc::{self, Pandoc, PandocOutput, PandocError};
 use std::error::Error;
 use utils::get_exe_dir;
 use std::path::PathBuf;
+use config::{Config, References};
 
 
-fn execute_pandoc(input: String, csl_dir: Option<PathBuf>) -> Result<PandocOutput, PandocError> {
+fn execute_pandoc(input: String, references: Option<References>) -> Result<PandocOutput, PandocError> {
     let mut renderer = Pandoc::new();
     renderer.set_output_format(pandoc::OutputFormat::Html, vec![]);
     renderer.set_input_format(pandoc::InputFormat::Markdown, vec![]);
@@ -13,12 +14,17 @@ fn execute_pandoc(input: String, csl_dir: Option<PathBuf>) -> Result<PandocOutpu
     renderer.add_option(pandoc::PandocOption::Smart);
     renderer.add_option(pandoc::PandocOption::Standalone);
     renderer.add_pandoc_path_hint(&get_exe_dir());
+    if references.is_some() {
+        let References { csl, bibliography } = references.unwrap();
+        renderer.set_bibliography(&bibliography);
+        renderer.set_csl(&csl);
+    }
     return renderer.execute();
 }
 
 
-pub fn render(input: String) -> Result<String, String> {
-    let output = execute_pandoc(input, None);
+pub fn render(config: Config, input: String) -> Result<String, String> {
+    let output = execute_pandoc(input, config.references);
     if output.is_err() {
         return Err(output.err().unwrap().description().into());
     }
