7e52f03e53c0192cc41a0e2931c26847a99920fb
Jake Howard <git@theorangeone.net>
2017-09-13 15:42:47 +0100

    Force UTF-8 charset

--- src/processors/head_cleanup.rs
+++ src/processors/head_cleanup.rs
@@ -1,4 +1,5 @@
 use html::{sciter_start, get_html, destroy_matching, destroy_at, get_head};
+use sciter::Element;
 use config::Config;
 
 
@@ -9,5 +10,7 @@ pub fn head_cleanup(config: Config, input: String) -> Result<String, String> {
     destroy_matching(&mut head, "meta[content='text/css']");
     destroy_matching(&mut head, "style");
     destroy_matching(&mut head, "title");
+    let mut meta_charset = Element::create_at("meta", &mut head).unwrap();
+    meta_charset.set_attribute("charset", "UTF-8");
     return Ok(get_html(root));
 }
