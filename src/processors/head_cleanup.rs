97dc08acc504bf7887b037a986c0ef020ed941a5
Jake Howard <git@theorangeone.net>
2017-09-24 16:22:56 +0100

    Make sciter find_all actually work

--- src/processors/head_cleanup.rs
+++ src/processors/head_cleanup.rs
@@ -6,7 +6,6 @@ use config::Config;
 pub fn head_cleanup(config: Config, input: String) -> Result<String, String> {
     let mut root = sciter_start(input);
     let mut head = get_head(&mut root);
-    destroy_at(&mut head, 4); // Sciter doesnt like finding this style tag for some reason
     destroy_matching(&mut head, "meta[content='text/css']");
     destroy_matching(&mut head, "style");
     destroy_matching(&mut head, "title");
