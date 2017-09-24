a6d6185f9f17c426c0999c28f82577e58806b67a
Jake Howard <git@theorangeone.net>
2017-09-24 17:28:15 +0100

    Set body class

--- src/processors/head_cleanup.rs
+++ src/processors/head_cleanup.rs
@@ -1,4 +1,4 @@
-use html::{sciter_start, get_html, destroy_matching, destroy_at, get_head};
+use html::{sciter_start, get_html, destroy_matching, destroy_at, get_head, get_body};
 use sciter::Element;
 use config::Config;
 
@@ -9,7 +9,12 @@ pub fn head_cleanup(config: Config, input: String) -> Result<String, String> {
     destroy_matching(&mut head, "meta[content='text/css']");
     destroy_matching(&mut head, "style");
     destroy_matching(&mut head, "title");
+
     let mut meta_charset = Element::create_at("meta", &mut head).unwrap();
     meta_charset.set_attribute("charset", "UTF-8");
+
+    let mut body = get_body(&mut root);
+    body.set_attribute("class", "content").expect("Failed to set body class");
+
     return Ok(get_html(root));
 }
