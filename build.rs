e663bfee4e01388e016b876a8fc6926f790dbf84
Jake Howard <git@theorangeone.net>
2017-11-26 14:17:21 +0000

    Compile and embed scss

--- build.rs
+++ build.rs
@@ -1,12 +1,33 @@
 extern crate include_dir;
+extern crate rsass;
 
 use std::env;
 use std::path::Path;
 use include_dir::include_dir;
+use rsass::{OutputStyle, compile_scss};
+use std::fs::File;
+use std::io::{Read, Write};
 
-fn main() {
+const ASSETS_DIR: &str = "assets";
+const ASSET_KEY: &str = "SRC";
+const STATIC_DIR: &str = "static";
+
+fn build_stylesheet() {
+    let mut in_file = File::open("static/style.scss").expect("Failed to open scss file");
+    let mut buffer = Vec::new();
+    in_file.read_to_end(&mut buffer).expect("Failed to read scss");
+    let compiled = compile_scss(buffer.as_slice(), OutputStyle::Compressed).expect("SCSS compiler failed");
+    let mut out_file = File::create("assets/style.css").expect("Failed to open css file");
+    out_file.write_all(&mut compiled.as_slice()).expect("Failed to write css to file");
+}
+
+fn embed_assets() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("assets.rs");
+    include_dir(ASSETS_DIR).as_variable(ASSET_KEY).to_file(dest_path).unwrap();
+}
 
-    include_dir("assets").as_variable("SRC").to_file(dest_path).unwrap();
+fn main() {
+    build_stylesheet();
+    embed_assets();
 }
